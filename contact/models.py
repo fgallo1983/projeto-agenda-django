ca6899e5719b4073cb6926a97c2a057e5c5fc13e
Felipe Gallo <felipe_gallo@yahoo.com>
2024-08-17 10:04:09 -0300

    Criando um ImageField e configurando MEDIA_URL e MEDIA_ROOT no Django

--- contact/models.py
+++ contact/models.py
@@ -6,8 +6,13 @@ from django.utils import timezone
 # email (email), created_date (date), description (text)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
+# category (foreign key), show (boolean)
 # picture (imagem)
+
+# Depois
+# owner (foreign key)
+
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -16,6 +21,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
